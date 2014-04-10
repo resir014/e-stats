b16124ca5a5b6e2909d40a1a1ddb7e0fe35219d4
Akshay Shekher <voldyman666@gmail.com>
2014-04-10 01:45:10 -0400

    Send email to any address specified in email.txt

--- cron.sh
+++ cron.sh
@@ -8,6 +8,6 @@ echo  "New Records: " `sqlite3 prod.db 'SELECT COUNT(*) FROM data WHERE rev=(SEL
 
 echo "Most Downloaded: " `sqlite3 prod.db 'SELECT name, MAX(downloads) FROM data WHERE rev=(SELECT MAX(rev) FROM dates)'` >> /tmp/email-data
 
-cat /tmp/email-data | mail -s "daily update" akshay.is.gr8@gmail.com
+cat /tmp/email-data | mail -s "daily update" `cat email.txt`
 
 rm /tmp/email-data
